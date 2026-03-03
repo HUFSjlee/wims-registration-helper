274db57bb075b3e33659080bb32af328ea76a9a2
deepsj1012 <deepsj1012@naver.com>
2026-03-03 15:31:35 +0900

    feat: init server

---
+++ server/wims-registration-helper-server/src/main/java/com/example/wimsregistrationhelperserver/WimsRegistrationHelperServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.wimsregistrationhelperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimsRegistrationHelperServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WimsRegistrationHelperServerApplication.class, args);
+    }
+
+}
---
+++ server/wims-registration-helper-server/src/test/java/com/example/wimsregistrationhelperserver/WimsRegistrationHelperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wimsregistrationhelperserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimsRegistrationHelperServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
